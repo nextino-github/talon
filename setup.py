45ea61798fa9e4d48013c16df1c62c0a604c242e
nextino-github <87066142+nextino-github@users.noreply.github.com>
2021-09-13 10:59:19 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -28,14 +28,14 @@ class InstallCommand(install):
             for not_required in ["numpy", "scipy", "scikit-learn==0.24.1"]:
                 dist.install_requires.remove(not_required)
 
-    def myversion():
+    def extract_version():
         from setuptools_scm import get_version
         return re.sub(r'\.[a-z]+.*$', '', get_version(root=os.path.dirname(__file__), relative_to=__file__, version_scheme='python-simplified-semver', local_scheme='no-local-version'))
 
 setup(name='talon',
       use_scm_version=True,
       setup_requires=['setuptools_scm'],
-      version=myversion(),
+      version=extract_version,
       description=("Mailgun library "
                    "to extract message quotations and signatures."),
       long_description=open("README.rst").read(),
